7ab1b9d19dc398a102e768d6ef330c8f40338558
Darunia18 <Darunia18@users.noreply.github.com>
2016-09-26 19:25:30 -0400

    Added lots of features
    -Added cooldown to prevent timeouts from Twitter
    -Changed behavior with retweeted statuses
    -Added exception handling to prevent crashes
    -I'm not sure what the if/else statement on line 29 is supposed to be for, but I believe I added it to @ the user who I replied to. This would be very annoying, so I never finished adding it and never removed the code.

--- DarApproved.py
+++ DarApproved.py
@@ -1,3 +1,4 @@
+import time
 import tweepy
 
 CONSUMER_KEY = ''
@@ -15,13 +16,28 @@ file.close()
 if last == '':
     last = '0'
 while True:
-    tweets = api.user_timeline("Darunia18")
-    for twt in tweets:
-        if twt.id > int(last):
-            last = twt.id
-            api.update_with_media("DarApproved.png","@Darunia18",in_reply_to_status_id=last)
-            print(twt.text)
-            file = open("last.txt",'w')
-            file.write(str(last))
-            file.close()
-            print("Approved! Waiting for Darunia18 to tweet again.")
+    try:
+        print("Running DarApproved...")
+        tweets = api.user_timeline("Darunia18")
+        for twt in reversed(tweets):
+            if twt.id > int(last):
+                try:
+                    user_displayname = twt.retweeted_status.user.screen_name
+                    tweet_id = twt.retweeted_status.id_str
+                    api.update_with_media("DarApproved.png","@Darunia18 http://twitter.com/"+user_displayname+"/status/"+tweet_id)
+                except:
+                    if twt.in_reply_to_user_id_str is not None:
+                        user_displayname = twt.in_reply_to_user_id_str
+                        api.update_with_media("DarApproved.png","@Darunia18",in_reply_to_status_id=twt.id)
+                    else:
+                        api.update_with_media("DarApproved.png","@Darunia18",in_reply_to_status_id=twt.id)
+                print(twt.text)
+                last = twt.id
+                file = open("last.txt",'w')
+                file.write(str(last))
+                file.close()
+                print("Approved! Waiting for Darunia18 to tweet again.")
+    except:
+        print("An error occurred! Trying again soon!")
+    print("Sleeping...")
+    time.sleep(36)
